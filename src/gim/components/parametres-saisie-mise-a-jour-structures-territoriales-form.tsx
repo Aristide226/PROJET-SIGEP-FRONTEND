b71d9f75c2e54b69f579629e5bf3ec694ec370e6
Aristide226 <ouagrawaaristide@gmail.com>
2026-08-10 18:40:39 +0000

    maj src/gim/components/parametres-saisie-mise-a-jour-structures-territoriales-form.tsx

--- src/gim/components/parametres-saisie-mise-a-jour-structures-territoriales-form.tsx
+++ src/gim/components/parametres-saisie-mise-a-jour-structures-territoriales-form.tsx
@@ -2,6 +2,16 @@
 
 const ParametresSaisieMiseAjourStructuresTerritorialesForm =()=> {
 
+
+    //////////GESTION REGIONS//////////
+
+
+    //////////GESTION PROVINCES//////////
+
+
+    //////////GESTION DEPARTEMENTS//////////
+
+    
     return (
         <div style={{width:'60%' , margin:'0 auto'}}>
 
